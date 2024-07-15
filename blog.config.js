4eb60c94b116cb1a8adb8f5cea6b558c23a3c49b
vivi <145866021+Soloveld@users.noreply.github.com>
2024-07-16 01:10:38 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,8 +26,8 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Avina', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'A beauty', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://avina-vv.lol', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, Blog', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://photos.avina-vv.lol', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Photography, Blog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'solovled188@gmail.com', // 邮箱地址 例如mail@tangly1024.com
